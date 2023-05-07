c742c0574e7e31b2a1d0a0f9f37de713d3440f39
Muhammad Umer Khan <umerkhan236242@gmail.com>
2023-05-07 20:17:14 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import Lottie from 'lottie-react';
+import Loading from './loading.json';
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import './index.css';
 import Main from './Main';
-import About from './components/About';
+import About from './About';
 import TeckStack from './components/TeckStack';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 function App() {
+  const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 5000);
+    }, []);
   return (
-    <div>
+    <>
+        {loading && <div className='items-center w-1/2 absolute left-1/4'>
+            <Lottie animationData={Loading} />
+            </div>}
+    {!loading && <div>
       <BrowserRouter>
       <Navbar />
       <div>
@@ -22,7 +36,8 @@ function App() {
       </Routes>
       </div>
       </BrowserRouter>  
-    </div>
+    </div>}
+    </>
   );
 }
 
--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import About from './components/About';
+import About from './About';
 import TeckStack from './components/TeckStack';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 
 const Main = () => {
     return (
-        <div>
+        <div className='bg-slate-900'>
             <About />
             <TeckStack />
             <Projects />

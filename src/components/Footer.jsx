e51564ff35598a49837032be32cfe4c0f3e8935a
umerkhan23 <umerkhanmcs23@gmail.com>
2024-03-23 15:55:38 +0500

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div className='relative top-20 bg-slate-950 p-3'>
-            <h1 className='text-white text-sm px-10 text-center'>©2023, Muhammad Umer Khan. All Rights Reserved</h1>
-        </div>
-    );
+  return (
+    <div className="relative top-20 bg-slate-950 p-3">
+      <h1 className="text-white text-sm px-10 text-center">
+        ©2024, Muhammad Umer Khan. All Rights Reserved
+      </h1>
+    </div>
+  );
 };
 
 export default Footer;
